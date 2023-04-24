5b8301bcc9f86a773dcf1ef7d973ed91f8472115
maell <mael.lecoeuche@gmail.com>
2023-04-24 23:35:32 +0200

    Commentaires menu.h

--- MiniProjet/Menu.h
+++ MiniProjet/Menu.h
@@ -1,13 +1,19 @@
+//Lecoeuche
 #pragma once
 #include <iostream>
 using namespace std;
 #include <string>
+
+//Menu de base permettant de lancer une partie ou d'en charger une ancienne, d'afficher les règles
 class Menu
 {
 	const string path = "C:/";
-public: 
+public:
+	//constructeur de menu
 	Menu();
+	//Instancie une nouvelle partie avec des valeurs de départ standarts
 	void NouvellePartie();
+	//Instancie une nouvelle partie avec des valeurs de départs issues des fichiers de sauvegarde
 	void ChargerPartie();
 };
 
